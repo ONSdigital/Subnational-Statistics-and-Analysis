7ca734e6c8a31e9266fcb41b0bd7608b486b3e1a
Haste <Will.Haste@ons.gov.uk>
2024-05-01 09:18:47 +0100

    code_for_publication

--- clustering/README.md
+++ clustering/README.md
@@ -1,26 +1,41 @@
-# Cloning the repository
-To be able to run this code, you will need to clone this git repository. To do this, run the following command:
-`git clone https://github.com/ONSdigital/Subnational-Statistics-and-Analysis.git`\
-This code was developed in a Vertex AI workbench where it is executable with Python notebooks, however if you wish to run the
-code another way it is likely that you will need to clone this repository into the cloud shell and deploy it to your chosen
-service from there.
+# Clustering code
 
-# Using this code
-The code within the clustering folder should be executed in a Google Cloud environment as it uses functions to get data from
-the BigQuery service. Throughout the code, there are instances of queries used to get the data that must be updated to match the
-path to your data in BigQuery. For example `project.ingest_dataset_name.table_name` would need to be updated in the code to the name of
-your project, followed by the name of the dataset and table that contains the data you will be running the clustering method on.
+See the below for a guide to the different files within the clustering refactor repository. If you run the code as specified with suitable data all outputs and model information will automatically be generated.
 
-# Running the headline clustering model
-To run the clustering model, you must have completed the above steps to copy the code to your notebook on LUDA. The code for running the model is held in the `mission_headline_support.ipynb`
-notebook. The execution of the headline model produced by this file works as follows:
-| Step          | Description |
-|-------------  | ----------- |
-|1              | Load subnational indicators explorer data from BigQuery. |
-|2              | Assign loaded metric variables to lists for their respective missions e.g. `mission1 = [gva, weekly_pay, employ_rate, gdi]`. Each loaded metric is a dataframe. |
-|3              | Clean each group of metrics: <ul><li>Convert upper tier local authorities (combination of counties and unitary authorities) to lower tier (singular unitary authorities or local authority districts).</li><li>Drop `index` and `YEAR` columns from data.</li><li>Rename `area` column to `AREACD` and put at the start of columns.</li><li>Convert all values to numeric.</li></ul> |
-|4              | Create the chosen clustering model. The code currently supports the following models: <ul><li>k-means</li></ul> |
-|5              | Combine group of metrics into one table. |
-|6              | Calculate clusters using specified clustering model. |
-|7              | Create radar plot, scatter plot and map of clusters using the `clusters_and_plots` or `clusters_and_plots_cb` (colourblind) functions. |
-</ol>
+## Notebooks/Run_clustering_script.ipynb
+This notebook calls in the functions and information from the other script and runs the clustering model. As it is currently set up it will run a clustering model on the data before outputting the radar plot, maps and supplementary model information. There are notes in each cell which detail what the code is doing and provides information on where you can customize the parameters of models.
+
+## Config.yaml
+This, aside from the run_clustering_script, is the only file you need to alter to run the model. In this file you specify file paths, the names of columns in the lookup files and the metrics you want to include in the model. There are notes within the script to guide you through this and the file must be saved before the model is run.
+
+## Cluster_code/Cluster_functions.py
+This script contains the functions involved in running the clustering model, mapping and exporting the outputs. These functions are called in the “run_clustering_script” notebook. All functions have docstrings detailing the inputs and outputs. Some functions have multiple outputs which must be allocated, these are detailed in the notes within the “run_clustering_script” notebook.
+
+## Data_prep/LAD23_boundaries_external.py
+This script contains the functions used to take data produced with old Local Authority boundaries and impute values for the newer Local Authorities. All functions have docstrings detailing the inputs and outputs.
+
+## Data_prep/subnat_data_clean.py
+This script contains all of the functions to clean the data, these functions are called and applied to the data in the “run_clustering_script” notebook. All functions have docstrings detailing the inputs and outputs.
+
+## Data_prep/subnat_data_import.py
+This script contains all of the functions to import the data, these functions are called and applied to the data in the “run_clustering_script” notebook. All functions have docstrings detailing the inputs and outputs.
+
+## Utils.py
+This script contains the “get_table_from_path” function which loads in data in the correct format.
+
+## Outputs
+The maps and data tables are output to this folder when the code is run.
+
+## Data
+A folder where you paste your data/the existing dataset is stored. The data must be in the same format as the example dataset in the folder. If there is a missing local authority district in your data and you want the value to be imputed there must be a row in the data for that area with a blank value column
+
+## Lookups
+The files in this folder include:
+-	Shapefiles for our mapping function.
+-	A lower-tier to upper-tier local authority lookup, used in our data cleaning function to impute results where lower-tier statistics are missing but upper-tier are available.
+-	Census population by age and geographical area datasets, used in our function to apportion old local authority results to the new local authority boundaries.
+-	A table of lower-tier local authority names, codes and ITL1 region. This dataset is used in the functions to create summary tables of the clusters.
+
+For more detail on the approach we have taken in this analysis, please see our [methodology article](https://www.ons.gov.uk/peoplepopulationandcommunity/wellbeing/methodologies/clusteringsimilarlocalauthoritiesintheukmethodology)
+
+For any queries regarding this code please contact subnational@ons.gov.uk
--- clustering/data_prep/subnat_data_clean.py
+++ clustering/data_prep/subnat_data_clean.py
@@ -1,92 +1,433 @@
-#Functions to read in and clean data from the subnational indicators explorer.
-def get_code_column(dataset, flag='E0'):
-    """Usage: The flag is a substring that we want to identify geography codes. 
-    Default is E0, which is the prefix of areas in England, 
-    but can be specified otherwise if data isn't at that level."""
+from typing import Dict, Sequence
+
+import pandas as pd
+import numpy as np
+import re
+
+from utils import get_table_from_path
+
+
+def get_code_column(
+    df: pd.DataFrame,
+    flag: str = 'E0',
+) -> str:
+    """Gets codes that start with the substring specified.
+
+    Parameters
+    ----------
+    df
+        Contains the loaded data.
+    flag
+        Substring to filter area codes by.
+
+    Returns
+    -------
+    Name of column with matching substring.
+    """
     #Note use of groupby here as there was a deprecation warning for all(level=1), suggesting groupby(level=1).all() is safer.
     #But beware that default behaviour of groupby is to sort alphabetically, which we very much don't want!
-    col = dataset.columns[dataset.stack().str.contains(flag).groupby(level=1, sort=False).any()]
-    if len(col)==0:
+    col = pd.Index(["AREACD"])
+    for column in df.columns:
+        if df.loc[df[column].astype(str).str.contains(flag), column].any():
+            col = pd.Index([column])
+    if len(col) == 0:
         print("ERROR: No columns that look like the contain geography codes found. Checking if the flag entered matches the expected pattern")
-    return(col)
 
-#Find the number of areas
-def number_areas(dataset, flag='E0'):
-    area_col = get_code_column(dataset, flag)
-    if len(area_col)==0:
-        return(0)
-    areas = dataset[area_col].drop_duplicates()
-    return(len(areas))
+    return col
 
-#Want to have methods to convert variables between upper tier and lower tier local authorities.
-def get_UTLT_lookup():
-#We're defining upp_low_tier lookup as a GLOBAL variable, as we don't want to read this unless is isn't defined.
-#Note here: this global is really module global, so defined for functions in this script.
-    global upp_low_tier
-    import pandas as pd
-    upp_low_tier = pd.read_csv("lookup/Lower_Tier_Local_Authority_to_Upper_Tier_Local_Authority__April_2019__Lookup_in_England_and_Wales.csv")
-    upp_low_tier = upp_low_tier[["LTLA19CD", "UTLA19CD"]]
-    global all_UT
-    all_UT = upp_low_tier["UTLA19CD"].drop_duplicates()
-        
-#Simple way to model this would be when we have UT data, model LT data as all equal to UT.
-def UT_metric_to_LT(metric):
-    import pandas as pd
-    lower_metrics=[]
+
+def number_areas(
+    df: pd.DataFrame,
+    flag: str = 'E0',
+) -> int:
+    """Gets the number of unique areas in the area code column.
+
+    Parameters
+    ----------
+    df
+        Contains the loaded data.
+    flag
+        Substring to filter area codes by.
+
+    Returns
+    -------
+    Number of unique area codes.
+    """
+    area_col = get_code_column(df, flag)
+    if len(area_col) == 0:
+        return 0
+    areas = df[area_col].drop_duplicates()
+    return len(areas)
+
+
+def get_all_upper_tier_la(
+    upper_to_lower_tier_lookup: pd.DataFrame,
+    upper_tier_col: str,
+) -> pd.Series:
+    """Gets a series of all upper tier local authorities from the lookup data.
+
+    Parameters
+    ----------
+    upper_to_lower_tier_lookup
+        Contains the upper to lower tier lookup data.
+    upper_tier_col
+        Name of column containing upper tier local authority codes.
+
+    Returns
+    -------
+    Series containing upper-tier local authority codes.
+    """
+    df = upper_to_lower_tier_lookup.loc[:, upper_tier_col].drop_duplicates()
+    return df
+
+
+def UT_metric_to_LT(
+    metric: pd.DataFrame,
+    upper_to_lower_tier_lookup: pd.DataFrame,
+    upper_tier_col: str,
+    lower_tier_col: str,
+    loaded_config: Dict,
+) -> pd.DataFrame:
+    """Converts upper tier local authorities to lower tier for a given metric.
+
+    Parameters
+    ----------
+    metric
+        Contains the data for a given metric.
+    upper_to_lower_tier_lookup
+        Contains the upper to lower tier lookup data.
+    upper_tier_col
+        Name of column containing upper tier local authority codes.
+    lower_tier_col
+        Name of column containing lower tier local authority codes.
+
+    Returns
+    -------
+    DataFrame with lower tier local authorities.
+    """
+    upper_tier_col= loaded_config["upper_tier_code_column_name"]
+    lower_tier_col= loaded_config["lower_tier_code_column_name"]
+    lower_metrics = []
     area_col = get_code_column(metric)
-    #Ensure we have the lookup of upper tier LAs to lower tier LAs in our environment.
-    if 'upp_low_tier' not in globals():
-        get_UTLT_lookup()
+    all_UT = get_all_upper_tier_la(upper_to_lower_tier_lookup, upper_tier_col)    
     for row in range(len(metric)):
         #Test to see if this a upper tier LA. If it is, replace it with lower tier.
         if metric.iloc[row].loc[area_col][0] in list(all_UT):
-            lower_metric = metric.reset_index().truncate(row,row).merge(upp_low_tier, 
-                                                                        left_on=area_col[0], right_on="UTLA19CD",how="left")
-            lower_metric = lower_metric.drop(columns=[area_col[0], 'UTLA19CD'])
-            col = lower_metric.pop('LTLA19CD')
+            lower_metric = metric.reset_index().truncate(row,row).merge(upper_to_lower_tier_lookup,
+                                                                        left_on=area_col[0], right_on=upper_tier_col,how="left")
+            lower_metric = lower_metric.drop(columns=[area_col[0], upper_tier_col])
+            col = lower_metric.pop(lower_tier_col)
             lower_metric.insert(1, area_col[0], col)
             lower_metrics.append(lower_metric)
         else:
-            lower_metric = metric.reset_index().truncate(row,row).merge(upp_low_tier, left_on=area_col[0], right_on="UTLA19CD", how="left")
-            lower_metric = lower_metric.drop(columns=['LTLA19CD', 'UTLA19CD'])
+            lower_metric = metric.reset_index().truncate(row,row).merge(upper_to_lower_tier_lookup, 
+                                                                        left_on=area_col[0], right_on=upper_tier_col, how="left")
+            lower_metric = lower_metric.drop(columns=[lower_tier_col, upper_tier_col])
             lower_metrics.append(lower_metric)
-    return(pd.concat(lower_metrics))
+    
+    return pd.concat(lower_metrics)
 
-#Function which drops an unwanted column from a dataframe.
-def drop_index_column(metric, col_to_drop='index'):
-    if col_to_drop in list(metric):
-        return(metric.drop(columns=col_to_drop))
-    else:
-        return(metric)
 
-#This takes the area column, renames in to 'AREACD' as in consolidated dataset. 
-#And sticks it in the front of the dataframe, in case it isn't already
-def harmonise_area_col_name(metric):
-    area_col=get_code_column(metric)[0]
-    col = metric.pop(area_col)
-    metric.insert(0, "AREACD", col)
-    return(metric)
+def drop_index_column(
+    df: pd.DataFrame,
+    col_to_drop: str = 'index',
+) -> pd.DataFrame:
+    """Drops the specified column from data.
 
-#Checks if the values are properly numeric and coerces them if they happen to be stringy.
-def ensure_value_numeric(metric):
-    import numpy as np
-    import pandas as pd
-    if not np.issubdtype(metric['Value'].dtypes, np.number):
-        metric['Value'] = pd.to_numeric(metric['Value'], errors='coerce')
-    return(metric)
+    Parameters
+    ----------
+    df
+        Contains the loaded data.
+    col_to_drop
+        Name of column to remove.
 
-#All in one function that does all the cleaning. Should work fine, as data that is OK should be left untouched by all components.
-def all_cleaning(metric):
-    metric = UT_metric_to_LT(metric)
-    metric = drop_index_column(metric)
-    metric = drop_index_column(metric, col_to_drop = 'YEAR')
-    metric = harmonise_area_col_name(metric)
-    metric = ensure_value_numeric(metric)
-    return(metric)
+    Returns
+    -------
+    DataFrame with column removed.
+    """
+    if col_to_drop in df.columns:
+        df = df.drop(columns=col_to_drop)
 
-def clean_groups(group):
+    return df
+
+
+def harmonise_area_col_name(
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+    """Renames area code column to AREACD and puts at front of DataFrame.
+
+    Parameters
+    ----------
+    df
+        Contains the loaded data.
+
+    Returns
+    -------
+    Formatted DataFrame.
+    """
+    area_col=get_code_column(df)[0]
+    col = df.pop(area_col)
+    df.insert(0, "AREACD", col)
+
+    return df
+
+
+def ensure_value_numeric(
+    df: pd.DataFrame,
+    value_col: str = "Value"
+):
+    """Changes value column to numeric type.
+
+    Parameters
+    ----------
+    df
+        Contains the loaded data.
+    value_col
+        Name of column containing values to be made numeric.
+
+    Returns
+    -------
+    DataFrame with specified column changed to numeric type.
+    """
+    if not np.issubdtype(df[value_col].dtypes, np.number):
+        df.loc[:, value_col] = pd.to_numeric(df[value_col], errors='coerce')
+
+    return df
+
+
+def all_cleaning(
+    loaded_config: Dict,
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+    """Cleans data for a given metric.
+
+    Parameters
+    ----------
+    loaded_config
+        Contains the loaded config.
+    df
+        Contains the loaded data.
+
+    Returns
+    -------
+    DataFrame with cleaned data.
+    """
+    ut_to_lt_lookup = get_table_from_path(
+        table_name=loaded_config["upper_tier_to_lower_tier_lookup"],
+        run_locally=loaded_config["run_locally"],
+        path=loaded_config["local_file_path"],
+        create_geodataframe=False,
+        cols_to_select=[loaded_config["upper_tier_code_column_name"], loaded_config["lower_tier_code_column_name"]],
+        project_name=loaded_config["gcp_project_name"],
+        dataset_name="ingest_luda",
+        project_location=loaded_config["gcp_project_location"],
+    )
+    #all unique area codes and names
+    df_cdlm = df[["AREACD", "AREANM"]]
+    df = UT_metric_to_LT(
+         metric = df,
+         upper_to_lower_tier_lookup=ut_to_lt_lookup,
+         loaded_config=loaded_config,
+         upper_tier_col= loaded_config["upper_tier_code_column_name"],
+         lower_tier_col= loaded_config["lower_tier_code_column_name"],
+    )
+    df = drop_index_column(df)
+    df = drop_index_column(df, col_to_drop='YEAR')
+    df = harmonise_area_col_name(df)
+    df = ensure_value_numeric(df)
+  #Separates out empty values, imputes and then appends to dataset
+    df_missing_values=df.loc[df["Value"].isna(), ]
+    df_missing_values=df_missing_values.drop("Value", axis = 1)
+    df_missing_values=df_missing_values.merge(df[["AREACD", "Value"]], on = "AREACD", how = "left")
+    df_missing_values=df_missing_values.loc[df_missing_values["Value"].notna(),]
+    df=pd.concat([df, df_missing_values])
+    df=df.merge(df_cdlm,on = ["AREACD", "AREANM"], how = "inner")
+    df=df.loc[df["Value"].notna(), ]
+
+    
+    return df
+
+def hle_cleaning(
+    loaded_config: Dict,
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+    """Cleans data for hle metrics(special case compared to the others).
+
+    Parameters
+    ----------
+    loaded_config
+        Contains the loaded config.
+    df
+        Contains the loaded data.
+
+    Returns
+    -------
+    DataFrame with cleaned data.
+    """
+    ut_to_lt_lookup = get_table_from_path(
+        table_name=loaded_config["upper_tier_to_lower_tier_lookup"],
+        run_locally=loaded_config["run_locally"],
+        path=loaded_config["local_file_path"],
+        create_geodataframe=False,
+        cols_to_select=[loaded_config["upper_tier_code_column_name"], loaded_config["lower_tier_code_column_name"]],
+        project_name=loaded_config["gcp_project_name"],
+        dataset_name="ingest_luda",
+        project_location=loaded_config["gcp_project_location"],
+    )
+    df = UT_metric_to_LT(
+        df,
+        upper_to_lower_tier_lookup=ut_to_lt_lookup,
+        upper_tier_col=loaded_config["upper_tier_code_column_name"],
+        lower_tier_col=loaded_config["lower_tier_code_column_name"],
+    )
+    df = drop_index_column(df)
+    df = drop_index_column(df, col_to_drop='YEAR')
+    df = harmonise_area_col_name(df)
+    df = ensure_value_numeric(df)
+    return df
+
+
+def clean_groups(
+    loaded_config: Dict,
+    group: Sequence[pd.DataFrame],
+) -> Sequence[pd.DataFrame]:
+    """Cleans multiple metrics.
+
+    Parameters
+    ----------
+    loaded_config
+        Contains the loaded config.
+    group
+        List of metric DataFrames.
+
+    Returns
+    -------
+    sequence of cleaned metric DataFrames.
+    """
+
     for metric in range(len(group)):
-        group[metric] = all_cleaning(group[metric])
-    return(group)
+        if metric == "Male healthy life expectancy" or metric == "Female healthy life expectancy":
+            group[metric] = hle_cleaning(loaded_config, group[metric])
+        else:
+            group[metric] = all_cleaning(loaded_config, group[metric])
+        
+
+    return group
+
+
+def winsorze(
+    df: pd.DataFrame,
+    lower_threshold: float,
+    upper_threshold: float,
+) -> pd.DataFrame:
+    """
+    
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        dataframe containing raw metrics.
+    lower_threshold : float
+        percentile threshold for lower bound in 0-1 format.
+    upper_threshold : float
+        percentile threshold for upper bound in 0-1 format.
+
+    Returns
+    -------
+    df : pd.dataframe
+        dataframe of winsorized data.
+
+    """
+    df = df.set_index("AREACD")
+    df = df.clip(lower=df.quantile(lower_threshold), upper=df.quantile(upper_threshold), axis=1)
+    df = df.reset_index()
+    return df
+    
+
+def get_winsorization_thresholds(
+    df: pd.DataFrame,
+    lower_threshold: float,
+    upper_threshold: float,
+) -> pd.DataFrame:
+    """
+    
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        dataframe containing raw metrics.
+    lower_threshold : float
+        percentile threshold for lower bound in 0-1 format.
+    upper_threshold : float
+        percentile threshold for upper bound in 0-1 format.
+
+    Returns
+    -------
+    df_thresholds : pd.dataframe
+        Dataframe of the winsorization thresholds.
+
+    """
+    df = df.set_index("AREACD")
+    df_thresholds = df.quantile([lower_threshold, upper_threshold])
+    df_thresholds = df_thresholds.reset_index()
+    return df_thresholds
+
+def get_desired_geography(
+    loaded_config: Dict,
+    df: pd.DataFrame,
+    geography_col: str,
+) -> pd.DataFrame:
+    """
+    
+
+    Parameters
+    ----------
+    loaded_config : Dict
+        saved config containing model parameters.
+    df : pd.DataFrame
+        data frame containing all of the metrics.
+    geography_col : str
+        The name of the desired geography column in the lookup.
+
+    Returns
+    -------
+    df : pd.dataframe
+        A dataframe with all of the metrics at desired geography.
+
+    """
+    lookup = get_table_from_path(
+        table_name=loaded_config["Geog_mapper"],
+        run_locally=loaded_config["run_locally"],
+        path=loaded_config["local_file_path"],
+        create_geodataframe=False,
+        cols_to_select=[geography_col],
+        project_name=loaded_config["gcp_project_name"],
+        dataset_name=loaded_config["Geog_mapper"],
+        project_location=loaded_config["gcp_project_location"],
+    )
+    df = lookup.merge(df, right_on = 'AREACD', left_on = geography_col, how='left')
+    df = df.rename(columns={geography_col: 'AREACD'})
+    return df
+    
+def get_correlation_matrix(
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+    """
+    
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The data to be fed into the clustering model.
+
+    Returns
+    -------
+    df_corr : pd.dataframe
+        A correlation matrix for all selected variables
+
+    """
+    df = df.set_index("AREACD")
+    df_corr = df.corr()
+    return df_corr
 
 
